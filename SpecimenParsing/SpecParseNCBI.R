df8a5cf109e4d64330c25bcecc8060586c04130b
kwebbink_fmnh <kwebbink@fieldmuseum.org>
2017-11-03 22:46:40 +0000

    SpecParseNCBI.R created online with Bitbucket

---
+++ SpecimenParsing/SpecParseNCBI.R
@@ -0,0 +1,21 @@
+# Parse FMNH specimen numbers out of NCBI sequence data
+
+#install.packages("traits")
+library("traits")
+
+out <- ncbi_searcher(taxa="Xenophrys legkaguli", seqrange = "1:2000", fuzzy = T)
+out$specNo1 <- gsub("(.*) FMNH (\\d+)(\\s+.*)", "FMNH \\2", out$gene_desc) 
+
+
+# Expand to parse other institutions' specimen numbers:
+
+Taxon <- readline("Enter a Taxon with sequences in the NCBI database: ")
+Institution <- readline("Enter an institution acronym: ")
+SeqRange <- readline("Enter the sequence length range (e.g., 1:2000): ")
+
+
+out2 <- ncbi_searcher(taxa=Taxon, seqrange = SeqRange, fuzzy = T)
+
+out2$specNo1 <- gsub(paste0("(.*) ",Institution," (\\d+)(\\s+.*)"), 
+                     paste0(Institution," \\2"), 
+                     out2$gene_desc)
