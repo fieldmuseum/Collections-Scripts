e5dba7274ec2672c185cbf1bbbfd3d15d013a99f
kwebbink_fmnh <kwebbink@fieldmuseum.org>
2017-11-14 22:39:04 +0000

    added Taxon & Institution example

--- SpecimenParsing/SpecParseNCBI.R
+++ SpecimenParsing/SpecParseNCBI.R
@@ -9,8 +9,8 @@ out$specNo1 <- gsub("(.*) FMNH (\\d+)(\\s+.*)", "FMNH \\2", out$gene_desc)
 
 # Expand to parse other institutions' specimen numbers:
 
-Taxon <- readline("Enter a Taxon with sequences in the NCBI database: ")
-Institution <- readline("Enter an institution acronym: ")
+Taxon <- readline("Enter a Taxon with sequences in the NCBI database (e.g., Xenophrys legkaguli): ")
+Institution <- readline("Enter an institution acronym: (e.g., FMNH)")
 SeqRange <- readline("Enter the sequence length range (e.g., 1:2000): ")
 
 
