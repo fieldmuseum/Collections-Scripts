ff76490258c6975a8ab761a2048b1b35992aea61
kwebbink_fmnh <kwebbink@fieldmuseum.org>
2017-12-06 22:28:01 +0000

    ui.R edited

--- shinySolr/ui.R
+++ shinySolr/ui.R
@@ -7,31 +7,6 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       h2("Search Filters"),
-     
-      # numericInput("num1", "Results Cap",
-      #              min = 100, max = 1000, value = 500, step = 50),
-      
-      # dateRangeInput("datecol", "Year Collected Range (Specimen Timeline)",
-      #                start  = "2000-01-01",
-      #                end    = "2020-12-31",
-      #                min    = "1700-01-01",
-      #                max    = "2020-12-31",
-      #                format = "dd-mm-yyyy"),  # check format!
-      # 
-      # dateRangeInput("datemod", "Date Modified Range (Data Freshness)",
-      #                start  = "2017-01-01",
-      #                end    = "2017-12-31",
-      #                min    = "2000-01-01",
-      #                max    = "2020-01-01",
-      #                format = "yyyy-mm-dd"),
-      
-      # selectInput("pickcat", "Collections:",
-      #             list(`Anthropology` = c("Anthropology"),
-      #                  `Botany` = c("Botany"),
-      #                  `Zoology` = c("Amphibians and Reptiles", "Birds", "Fishes",
-      #                                "Insects", "Invertebrate Zoology", "Mammals")),
-      #             multiple = TRUE, # size = 3,
-      #             selected = c("Botany", "Birds")),
       
       # selectInput("pickcore", "Cores (this should be hidden/bkg & tied to 'Collections'):",
       #             list(`Anthropology` = c("core_ecatalogue_ant"),
@@ -39,13 +14,16 @@ shinyUI(fluidPage(
       #                  `Zoology` = c("core_ecatalogue_zoo"))),
       
       selectInput("pickfacet", "Fields/Facets:",
-                  multiple = TRUE, # size = 3,
-                  c("ss_ClaPhylum","ss_ClaClass","ss_ClaOrder","ss_ClaFamily","ss_ClaGenus"),
-                  selected = "ss_ClaGenus"),
-                       # `Where` = c("ss_DarCountry"),
-                       #`When` = c("DarYearCollected"))),
+                  multiple = FALSE, # size = 3,
+                  list(
+                    `What` = c("ss_DarCollectionCode", "ss_DarPhylum","ss_DarClass","ss_DarOrder","ss_DarFamily","ss_DarGenus"),
+                    `Where` = c("ss_DarContinentOcean", "ss_DarCountry", "ss_DarStateProvince"),
+                    `When` = c("ss_DarYearCollected", "ss_DarMonthCollected", "ss_DarYearIdentified"),
+                  selected = "ss_DarCollectionCode")),
 
-      textInput("text1", "Keywords", value="Ricciaceae")
+      textInput("text1", "Keywords", value="Ricciaceae"),
+      
+      submitButton("Submit")
     ),
     
     mainPanel(
@@ -53,14 +31,10 @@ shinyUI(fluidPage(
       fluidRow(
         column(width=7,
           plotOutput("plot1"),
+          plotOutput("plot2"),
           tableOutput("table1")),
         column(width=3,
           htmlOutput("image1"))
-          # code("some code??"),
-          # h3(textOutput("cap1")),
-          # textOutput("text1"),
-          # textOutput("filterCats"),
-          # textOutput("facetDates")
         )))
     )
   )
