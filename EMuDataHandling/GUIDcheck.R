eb2eee6cc9e0052e89bcfdb24c96ae2b3b76ef20
Kate W <magpiedin@gmail.com>
2023-01-31 17:25:49 -0600

    handle emultimedia and duplicate-irn's

--- EMuDataHandling/GUIDcheck.R
+++ EMuDataHandling/GUIDcheck.R
@@ -3,15 +3,16 @@
 #
 # Setup:
 # 1. In EMu, set up a CSV UTF-8 report with either:
-#   Option 1: a group of these 4 columns:
+#   Option 1: a group of these four columns:
 #     - irn
 #     - AdmGUIDIsPreferred_tab
 #     - AdmGUIDType_tab
 #     - AdmGUIDValue_tab
 #
-#   Option 2: these 2 columns:
+#   Option 2: Two of these columns, as appropriate:
 #     - irn
-#     - DarGlobalUniqueId
+#     - DarGlobalUniqueId (if checking ecatalogue)
+#     - AudIdentifier (if checking emultimedia)
 #
 #   ...See example in "EMuDataHandling/sample_data/GUIDcheck/"
 #
@@ -23,35 +24,43 @@
 #     "EMuDataHandling/real_data_in/GUIDcheck/Group1.csv"
 
 
-# install.packages("tidyr","dplyr","progress")
+# install.packages(c("readr","tidyr","dplyr","progress"))
 library("readr")
 library("tidyr")
 library("dplyr")
 library("progress")
 
-# point to your csv file
+
+#### Input - point to your csv file ####
+
 input_file <- "EMuDataHandling/real_data_in/GUIDcheck/Group1.csv"
 
 records <- read_csv(file=input_file,
                     progress = TRUE)
 
+
+#### Check input GUID field ####
+
 if ("AdmGUIDValue" %in% colnames(records)) {
-  
   colnames(records)[colnames(records)=="AdmGUIDValue"] <- "GUID"
   
 } else {
-  
   if ("DarGlobalUniqueIdentifier" %in% colnames(records)) {
-    
     colnames(records)[colnames(records)=="DarGlobalUniqueIdentifier"] <- "GUID"
     
   } else {
-    
-    print("Error -- Cannot find 'DarGlobalUniqueIdentifier' or grouped 'AdmGUIDValue' column in input CSV")
-    
+    if ("AudIdentifier" %in% colnames(records)) {
+      colnames(records)[colnames(records)=="AudIdentifier"] <- "GUID"
+      
+    } else {
+      print("Error -- Cannot find 'DarGlobalUniqueIdentifier', 'AudIdentifier', or grouped 'AdmGUIDValue' column in input CSV")
+      
+    }
   }
-
 }
+
+
+#### Count GUIDs ####
 
 if (NROW(records) > 1000) {
 
@@ -68,6 +77,20 @@ record_dups <- merge(records, guids_dups,
                      by="GUID",
                      all.y = TRUE)
 
+record_dups <- unique(record_dups[,c("irn","GUID","n")])
+
+
+#### Check output ####
+
+# irn's may be duplicated in reports that take a long time to run...
+# (specifically, irn's that were edited while the report was running.)
+re_check <- dplyr::count(record_dups, GUID)
+re_check <- re_check[re_check$n > 1,]
+record_dups <- record_dups[record_dups$GUID %in% re_check$GUID,]
+
+
+#### Output ####
+
 if (NROW(record_dups) > 0) {
   
   output_filename <- "EMuDataHandling/real_data_in/GUIDcheck/guid_dups.csv"
@@ -76,7 +99,7 @@ if (NROW(record_dups) > 0) {
               NROW(record_dups),"records to: "),
               output_filename))
 
-  write_csv(unique(record_dups[,c("irn","GUID","n")]),
+  write_csv(record_dups,
             output_filename)
 } else {
   
